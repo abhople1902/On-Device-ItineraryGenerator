a284a1ba864eea4f904d0d3234838a73adcd31fb
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2025-11-28 11:29:37 +0530

    Initial Commit

---
+++ Foundation Itinerary/Foundation_ItineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foundation_ItineraryApp.swift
+//  Foundation Itinerary
+//
+//  Created by Ayush Bhople on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Foundation_ItineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Foundation ItineraryTests/Foundation_ItineraryTests.swift
@@ -0,0 +1,16 @@
+//
+//  Foundation_ItineraryTests.swift
+//  Foundation ItineraryTests
+//
+//  Created by Ayush Bhople on 28/11/25.
+//
+
+import Testing
+
+struct Foundation_ItineraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
