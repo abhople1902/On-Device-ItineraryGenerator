e345267e4027b9fed82bbeeff55723bd5db0ecd6
Ayush Bhople <77747115+abhople1902@users.noreply.github.com>
2025-11-28 11:41:40 +0530

    Pilot Foundation models framework

---
+++ Foundation Itinerary/Playground testing/Playgrounds.swift
@@ -0,0 +1,15 @@
+//
+//  Playgrounds.swift
+//  Foundation Itinerary
+//
+//  Created by Ayush Bhople on 28/11/25.
+//
+
+import Playgrounds
+import FoundationModels
+
+#Playground {
+    let session = LanguageModelSession()
+    
+    let response = try await session.respond(to: "Give an itinerary for a 4 day trip in Krabi.")
+}
